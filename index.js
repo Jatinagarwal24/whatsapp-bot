22bb5ff5d0305e55ffc1228a48c672df3bb992d2
Jatinagarwal24 <23110147@iitgn.ac.in>
2026-05-21 01:55:57 +0530

    fixed login issue

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ const cron = require('node-cron');
 
 // Initialize the WhatsApp Client
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu'
+        ]
+    }
 });
 
 const readline = require('readline');
